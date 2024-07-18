5e7245b31a578c78862794d501304ff43c232bfb
David Silva Santos Júnior <55060834+DavidSilvaSantosJr@users.noreply.github.com>
2024-07-18 07:46:16 -0300

    Add files via upload

---
+++ self_join.sql
@@ -0,0 +1,18 @@
+-- agrpa valores da mesma tabela
+
+/*
+select coluna
+from tabela a, tabela b
+where condição
+*/
+
+select A.ContactName, B.Region
+from dbo.Customers A, dbo.Customers B
+where A.Region = B.Region;
+
+-- encontrar (nome e data de contratação) de todos os funcionários que foram contratados no mesmo ano
+select N.FirstName, N.HireDate, H.FirstName, H.HireDate
+from dbo.Employees N, dbo.Employees H
+where datepart(year, N.HireDate) = datepart(year, H.HireDate);
+
+-- encontrar na tabela detalhe de pedido quais produtos	tem o mesmo percentual de desconto
