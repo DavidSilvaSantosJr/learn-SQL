4a3979953d073a713bb565d94c915b8da3b600b5
David Silva Santos Júnior <55060834+DavidSilvaSantosJr@users.noreply.github.com>
2024-07-22 16:47:03 -0300

    Update self_join.sql

--- self_join.sql
+++ self_join.sql
@@ -3,16 +3,21 @@
 /*
 select coluna
 from tabela a, tabela b
-where condiÁ„o
+where condi√ß√£o
 */
 
 select A.ContactName, B.Region
 from dbo.Customers A, dbo.Customers B
 where A.Region = B.Region;
 
--- encontrar (nome e data de contrataÁ„o) de todos os funcion·rios que foram contratados no mesmo ano
+-- encontrar (nome e data de contrata√ß√£o) de todos os funcion√°rios que foram contratados no mesmo ano
 select N.FirstName, N.HireDate, H.FirstName, H.HireDate
 from dbo.Employees N, dbo.Employees H
 where datepart(year, N.HireDate) = datepart(year, H.HireDate);
 
 -- encontrar na tabela detalhe de pedido quais produtos	tem o mesmo percentual de desconto
+ select a.ProductID, a.Discount, b.ProductID,b.Discount
+ from dbo.[Order Details] a, dbo.[Order Details] b
+ where a.Quantity = b.Quantity
+ order by a.ProductID
+
